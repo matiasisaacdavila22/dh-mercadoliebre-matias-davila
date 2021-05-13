c8dae37fd05d03a6be761affae0aad5aafb63608
matiasisaacdavila22 <mipcomputacion@gmail.com>
2021-05-13 12:12:52 -0300

    bugs heroku3

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -2,21 +2,21 @@
 const express = require('express');
 const path = require('path');
 const router = express.Router();
-const multer = require('multer');
+let multer = require('multer');
 
 const userController = require('../controllers/userController');
 
-const storage = multer.diskStorage({
+let storage = multer.diskStorage({
     destination: (req, file, cb) => {
         cb(null, path.join(__dirname, '../public/images/user'));
     },
     filename: (req, file, cb) => {
-        const newFileName = 'user-'+Date.now() + path.extname(file.originalname);
+        let newFileName = 'user-'+Date.now() + path.extname(file.originalname);
         cb(null, newFileName);
     }
 });
 
-const upload = multer({storage: storage});
+let upload = multer({storage: storage});
 
 router.get('/register', userController.register);
 router.post('/register', upload.single('file'), userController.create); 
